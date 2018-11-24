dbfd98beeab768489083be8ab8785b6641904389
Lucas De Souza <37718781+Luc4sD3Souz4@users.noreply.github.com>
2018-11-24 16:25:52 -0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+/*var myHeading = document.querySelector('h1');
+
+var form = document.getElementById('conta');
+var first = document.getElementById('first');
+var second = document.getElementById('second');
+var campo = document.getElementById('campo').value;*/
+
+
+
+var answer = 0;
+function bini() {
+    var first = document.getElementById('first');
+    var second = document.getElementById('second');
+    var campo = document.getElementById('campo').value;
+    // alerta o valor do campo
+    switch (campo) {
+        case "soma":
+            alert(parseFloat(second.value) + parseFloat(first.value));
+            break;
+            
+        case "subtracao":
+            alert(parseFloat(first.value) - parseFloat(second.value));
+            break;
+            
+        case "multiplicacao":
+            alert(parseFloat(first.value) * parseFloat(second.value));
+            break;
+           
+        case "divisao":
+            alert(parseFloat(first.value) / parseFloat(second.value));
+            break;
+            
+        case "exponenciacao":
+            alert(parseFloat(first.value) ^ parseFloat(second.value));
+            break;
+    }
+    
+}
+
+//var first = prompt("Digite o primeiro numero");
+//var second = prompt("Digite o segundo numero");
