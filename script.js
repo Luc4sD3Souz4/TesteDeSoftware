490fcc756a5d7bd9ea32cf661871051200c4d38f
Lucas De Souza <37718781+Luc4sD3Souz4@users.noreply.github.com>
2018-11-29 18:21:30 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-/*var myHeading = document.querySelector('h1');
-
-var form = document.getElementById('conta');
-var first = document.getElementById('first');
-var second = document.getElementById('second');
-var campo = document.getElementById('campo').value;*/
-
-
-
 var answer = 0;
-function bini() {
-    var first = document.getElementById('first');
-    var second = document.getElementById('second');
-    var campo = document.getElementById('campo').value;
-    // alerta o valor do campo
+function bini(numero1,numero2,campo) {
+
+   // alerta o valor do campo
+    var answer = 0;
     switch (campo) {
         case "soma":
-            alert(parseFloat(second.value) + parseFloat(first.value));
+            answer = (parseFloat(numero1) + parseFloat(numero2));
             break;
             
         case "subtracao":
-            alert(parseFloat(first.value) - parseFloat(second.value));
+            answer = (parseFloat(numero1) - parseFloat(numero2));
             break;
             
         case "multiplicacao":
-            alert(parseFloat(first.value) * parseFloat(second.value));
+            answer = (parseFloat(numero1) * parseFloat(numero2));
             break;
            
         case "divisao":
-            alert(parseFloat(first.value) / parseFloat(second.value));
+            answer = (parseFloat(numero1) / parseFloat(numero2));
             break;
             
         case "exponenciacao":
-            alert(parseFloat(first.value) ** parseFloat(second.value));
+            answer = (parseFloat(numero1) ** parseFloat(numero2));
             break;
     }
-    
+    return answer;   
 }
 
 //var first = prompt("Digite o primeiro numero");
